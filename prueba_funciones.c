a5ca866d54013bc270936844da8a8b5a3542457e
Iv4n0f <ilopezz@unsa.edu.pe>
2024-10-04 17:38:11 -0500

    prueba de flipH, genera un reflejo de la imagen

--- prueba_funciones.c
+++ prueba_funciones.c
@@ -1,8 +1,15 @@
 #include "chess.h"
 #include "figures.h"
 
+/*
 void display(){
   char** flip = flipV(knight);
   interpreter(flip);
 }
 
+*/
+
+void display(){
+  char ** flip_h = flipH(knight);
+  interpreter(flip_h);
+}
