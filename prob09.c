198c3e75f10d192dee3d473b20f7d4905597c870
CSV-Alex <acanapatana@unsa.edu.pe>
2024-10-06 17:17:46 -0500

    Avance: Generando la fila 2 del tablero del juego 3 de Fischer vs Spassky (1972)

--- prob09.c
+++ prob09.c
@@ -3,18 +3,26 @@
 
 void display(){
   char ** blackSquare = reverse(whiteSquare);
-  char ** board;
   char ** pairSquare = join(whiteSquare, blackSquare);
 
   // Variables Peon y su alterno
   char ** whitePawn = superImpose(pawn, whiteSquare);
   char ** alterPawn = superImpose(reverse(pawn), whiteSquare);
 
+  // Fischer vs Spassky: Game 3 (1972)
   // Fila 1
   char ** row1 = repeatH(pairSquare, 3);
   char ** blackKing = superImpose(reverse(king), whiteSquare);
   row1 = join(row1, blackKing);
   row1 = join(row1, blackSquare);
 
-  interpreter(row1);
+  // Fila 2
+  char ** row2 = reverse(repeatH(pairSquare, 2));
+  row2 = join(row2, blackSquare);
+  row2 = join(row2, alterPawn);
+  row2 = join(row2, reverse(pairSquare));
+
+  // Tablero
+  char ** board = up(row1, row2);
+  interpreter(board);
 }
