84e4db3707342343085cd8f62b00804debbe847c
CSV-Alex <acanapatana@unsa.edu.pe>
2024-10-05 22:58:56 -0500

    Avance: Dos filas con cuatro caballos con rotacion distinta

--- prob07.c
+++ prob07.c
@@ -5,13 +5,18 @@ void display() {
   // Patron de 3 cuadrados consecutivos
   char ** row3 = join(reverse(whiteSquare), whiteSquare);
   row3 = join(row3, reverse(whiteSquare));
-  
+
   // Creando el caballo y su rotado
   char ** knightBg = superImpose(knight, reverse(whiteSquare));
   char ** knightBgRot = superImpose(rotateR(knight), whiteSquare);
-  
+
   char ** baseRow = join(reverse(row3), knightBg);
   baseRow = join(baseRow, knightBgRot);
   baseRow = join(baseRow, row3);
-  interpreter(baseRow);
+
+  // Fila doble de caballos rotados
+  char ** baseRowRotated = rotateR(rotateR(baseRow));
+  char ** centerPattern = up(baseRow, baseRowRotated);
+
+  interpreter(centerPattern);
 }
