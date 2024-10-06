fba1830176f8847473e7f7b6534f580e014a3a10
Berly <bduenasm@unsa.edu.pe>
2024-10-05 21:37:38 -0500

    Avance P4: Fila base de piezas unidas de forma ordenada usando varios join consecutivos probados en el commit anterior

--- prob04.c
+++ prob04.c
@@ -14,5 +14,9 @@ void display(){
   char ** fRow = join(rook, knight);
   fRow = join(fRow, bishop);
   fRow = join(fRow, queen);
+  fRow = join(fRow, king);
+  fRow = join(fRow, bishop);
+  fRow = join(fRow, knight);
+  fRow = join(fRow, rook);
   interpreter(fRow);
 }
