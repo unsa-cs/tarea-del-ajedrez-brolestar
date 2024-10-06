633ed8ec401dffdd5627aa0684857458c15e45f9
Berly <bduenasm@unsa.edu.pe>
2024-10-05 21:56:54 -0500

    Avance: Usando la fila de piezas implementada en el problema 4 y usando una fila de peones con la funcion repeatH, generamos la posicion de las piezas del jugador de las piezas blancas

--- prob06.c
+++ prob06.c
@@ -6,5 +6,18 @@ void display() {
   char ** emptyRow = repeatH(dualBlock, 4);
   char ** emptyRow2 = reverse(emptyRow);
   char ** dualRow = up(emptyRow2, emptyRow);
-  interpreter(dualRow);
+
+  //Fila de piezas 
+  char ** fRow = join(rook, knight);
+  fRow = join(fRow, bishop);
+  fRow = join(fRow, queen);
+  fRow = join(fRow, king);
+  fRow = join(fRow, bishop);
+  fRow = join(fRow, knight);
+  fRow = join(fRow, rook);
+
+  char ** pRow = repeatH(pawn, 8);
+  char ** WhitePattern = up(fRow, pRow);
+
+  interpreter(WhitePattern);
 }
