d24d789b23ccb686299b2145fbadfe9fea7f2a5e
J0609 <jhuaracaq@unsa.edu.pe>
2024-10-06 18:33:46 -0500

    Avance: Unión de fila 1 y fila 2

--- prob10.c
+++ prob10.c
@@ -3,8 +3,28 @@
 
 void display(){
   char ** blackSquare = reverse(whiteSquare);
-
   char ** pairSquare = join(whiteSquare, blackSquare);
-  char ** emptyRow = repeatH(pairSquare,4);
+  char ** emptyRow = repeatH(pairSquare, 4);
+  
+  // Variables Peon y su alterno
+  char ** whitePawn = superImpose(pawn, whiteSquare);
+  char ** alterPawn = superImpose(reverse(pawn), whiteSquare);
+
+  // 1997 5ª partida, Kasparov contra Deep Blue
+
+  // Primera fila
+  char ** row1 = emptyRow;
+
+  // Segunda fila
+  char ** row2 = join(reverse(whitePawn), alterPawn);
+  row2 = join(row2, reverse(pairSquare));
+  row2 = join(row2, reverse(pairSquare));
+  row2 = join(row2, reverse(alterPawn));
+  row2 = join(row2, whiteSquare);
+
+  // Tablero
+  char ** board =  up(row1, row2);
+  interpreter(board);
+  
 }
 
