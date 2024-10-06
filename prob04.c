2eae2c17d94a329cd1dd25030434ae9618f1fd28
Jose Cari <jcarial@unsa.edu.pe>
2024-10-05 21:19:20 -0500

    Prueba: Join anidado para juntar tres piezas(torre, caballo y alfil)

--- prob04.c
+++ prob04.c
@@ -1,9 +1,16 @@
 #include "chess.h"
 #include "figures.h"
 
+/*
 void display(){
   char ** dualBlock = join(whiteSquare, reverse(whiteSquare));
   char ** emptyRow = repeatH(dualBlock, 4);
   char ** emptyRowReverse = reverse(emptyRow);
   interpreter(emptyRowReverse);
+}
+*/
+
+void display(){
+  char ** pieceTuple = join(rook, join(knight, bishop));
+  interpreter(pieceTuple);
 }
